6746e8bdc57c873c1c956b41063d3fe52ce5f428
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2023-12-24 02:24:56 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,7 @@ import sys
 
 def print_msg(dict_sc, total_file_size):
     """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
+   helper function to display stats
     """
 
     print("File size: {}".format(total_file_size))
