0db5c22d050cf1ba8f79e3120d2ec9e05a42f54c
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2024-01-05 03:17:02 +0100

    0x04-utf8_validation
    0x04-utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-""" UTF-8Validation"""
-
+"""UTF-8 Validation"""
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
-    UTF-8encoding...
+    Determine if a given data set represents a valid UTF-8 encoding.
     """
-    number_bytes = 0
-
+    remaining_bytes = 0
     mask_1 = 1 << 7
     mask_2 = 1 << 6
 
-    for i in data:
-
+    for byte in data:
         mask_byte = 1 << 7
 
-        if number_bytes == 0:
+        if remaining_bytes == 0:
+            # Count the number of leading 1s to determine the number of bytes
+            while mask_byte & byte:
+                remaining_bytes += 1
+                mask_byte >>= 1
 
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
+            if remaining_bytes == 0:
                 continue
 
-            if number_bytes == 1 or number_bytes > 4:
+            # Check if the number of bytes is valid
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
 
         else:
-            if not (i & mask_1 and not (i & mask_2)):
-                    return False
+            # Check if the byte follows the UTF-8 format
+            if not (byte & mask_1 and not (byte & mask_2)):
+                return False
 
-        number_bytes -= 1
+        remaining_bytes -= 1
 
-    if number_bytes == 0:
-        return True
+    return remaining_bytes == 0
 
-    return False
-    
+# Test cases
+data1 = [65]
+print(validUTF8(data1))
+
+data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data2))
+
+data3 = [229, 65, 127, 256]
+print(validUTF8(data3))
