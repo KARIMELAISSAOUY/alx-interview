27806d9ab50a063d271aa1f20a10b866383b6942
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2024-01-27 01:40:58 +0100

    0x07-rotate_2d_matrix
    0x07-rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,7 +1,9 @@
-    #!/usr/bin/python3
-    # Module for 2D Matrix Rotation.
+#!/usr/bin/python3
+"""Module for 2D Matrix Rotation.
+"""
 
-    def rotate_2d_matrix(matrix):
+
+def rotate_2d_matrix(matrix):
         """
         Rotates a 2D matrix 90 degrees clockwise in-place.
         Parameters:
